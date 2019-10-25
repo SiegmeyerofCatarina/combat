18f72f473d07206536dccb61603b10d8aa01f318
RS <Raido.sowl@gmail.com>
2019-10-25 18:47:39 +0500

    Update logger

--- logger.py
+++ logger.py
@@ -10,10 +10,20 @@ class Logger:
         :param args:
         :return:
         """
-
-
         print('generated {} persons'.format(len(persons)))
         teams = defaultdict(int)
         for person in persons:
             teams[person.team] += 1
         print(*map('{} in {} team'.format, teams.values(), teams.keys()), sep=', ')
+
+    def death(self, person: 'Entity'):
+        print('{} died!'.format(person.name))
+
+    def end_combat(self, alive: set()):
+        if alive:
+            # print('{} win!'.format(alive.winner))
+            print(*map('{} alive with {} hp'.format,
+                       [person.name for person in alive],
+                       [person.health.hp for person in alive]), sep=', ')
+        else:
+            print('All dead!')
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # main program call combat module with scene and entities
-from __future__ import annotations
-from collections import defaultdict
 from logger import Logger
 from entity import generate_entity, generate_scene
 
@@ -8,10 +6,9 @@ from entity import generate_entity, generate_scene
 def main():
     logger = Logger()
     scene = generate_scene()
-    persons = {*map(generate_entity, range(10))}
+    persons = {*map(generate_entity, range(3))}
     combat = Combat(scene, persons, logger)
     combat.do()
-    pass
 
 
 class Combat:
@@ -21,8 +18,7 @@ class Combat:
         :param scene:
         :param persons:
         """
-        print("Make war not love!")
-        self.logger = logger
+        self.log = logger
         self.scene = scene
         self.persons = persons
 
@@ -31,34 +27,32 @@ class Combat:
         lets fight!
         :return: winner
         """
-        self.logger.strart_combat(self.scene, self.persons)
+        self.log.strart_combat(self.scene, self.persons)
         mortuary = set()
         winner = False
-        persons_in_combat = self.persons
-        while not winner:
-            for person in persons_in_combat:
-                if person.health.alive:
 
-                    targets = persons_in_combat
+        while not winner:  # main loop
+            for person in self.persons:
+                if person.health.alive:
+                    targets = self.persons
                     person.do_action(targets)
                 else:
                     mortuary.add(person)
-                    print('{} died!'.format(person.name))
+                    self.log.death(person)
 
-            persons_in_combat -= mortuary
+            self.persons -= mortuary
+            winner = self.search_winner()
+        else:
+            self.log.end_combat(self.persons)
 
-            teams = set([person.team for person in persons_in_combat])
-            if len(teams) <= 1:
-                if len(teams) == 1:
-                    winner = self.persons.pop()
-                    print('{} win!'.format(winner.team))
-                    print(*map('{} alive with {} hp'.format,
-                               [person.name for person in persons_in_combat],
-                               [person.health.hp for person in persons_in_combat]), sep=', ')
-                else:
-                    winner = True
-                    print('all died!')
-        return winner
+    def search_winner(self):
+        teams = set([person.team for person in self.persons])
+        if len(teams) <= 1:
+            if len(teams) == 1:
+                winner = teams.pop()
+            else:
+                winner = True
+            return winner
 
 
 if __name__ == '__main__':
