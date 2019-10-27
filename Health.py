3d11409925aea187f32654c453be95e1f7edf60e
Artur <faartur@gmail.com>
2019-10-27 20:10:01 +0200

    Make Health getters and setters private

--- Health.py
+++ Health.py
@@ -24,35 +24,35 @@ class Health:
         self.__alive = alive
         self.__death_callback = death_callback
 
-    def get_alive(self) -> bool:
+    def __get_alive(self) -> bool:
         return self.__alive
 
-    def set_alive(self, state) -> None:
+    def __set_alive(self, state) -> None:
         self.__alive = state
         if not self.alive and self.__death_callback:
             self.__death_callback()
 
-    def get_health(self) -> int:
+    def __get_health(self) -> int:
         return self.__health
 
-    def set_health(self, value) -> None:
+    def __set_health(self, value) -> None:
         self.__health = min(max(value, 0), self.max_health)
-        self.check_alive()
+        self.__check_alive()
 
-    def get_health_in_percentage(self) -> float:
+    def __get_health_in_percentage(self) -> float:
         return round(self.health * 100 / self.max_health, 2)
 
-    def get_max_health(self) -> int:
+    def __get_max_health(self) -> int:
         return self.__max_health
 
-    def set_max_health(self, value) -> None:
+    def __set_max_health(self, value) -> None:
         self.__max_health = value
         self.health += self.max_health - value
 
-    def check_alive(self) -> None:
+    def __check_alive(self) -> None:
         self.alive = self.health > 0
 
-    alive = property(get_alive, set_alive)
-    health = property(get_health, set_health)
-    max_health = property(get_max_health, set_max_health)
-    health_in_percentage = property(get_health_in_percentage)
+    alive = property(__get_alive, __set_alive)
+    health = property(__get_health, __set_health)
+    max_health = property(__get_max_health, __set_max_health)
+    health_in_percentage = property(__get_health_in_percentage)
